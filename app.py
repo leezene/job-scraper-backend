758c896e6027827aa927773456a4feb874931a66
unknown <actionbishal98130@gmail.com>
2021-05-26 12:55:47 +0545

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
 from flask.helpers import send_file
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 import indeed_job_scraper, linkedin_job_scraper
 
 
